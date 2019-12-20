02cb952323b559300e7957a6a2f89f94807a181d
Kirill Kuvshinov <kkirka@gmail.com>
2019-12-20 18:36:41 +0300

    [SDAO-17] Fix modifier list order
    Problem: Solidity guidelines recommend putting visibility modifiers
    first in the modifiers list.
    
    Solution: Swap "public" and "initializable"

--- contracts/BlendToken.sol
+++ contracts/BlendToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract BlendToken is Initializable, ERC20, ERC20Detailed {
-    function initialize(uint256 initialSupply) initializer public {
+    function initialize(uint256 initialSupply) public initializer {
         ERC20Detailed.initialize("Blend Token", "BLEND", 18);
         _mint(msg.sender, initialSupply);
     }

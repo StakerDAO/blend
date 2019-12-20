b4875a57f680ffa2fc0e719e57afbf37849b42ba
Kirill Kuvshinov <kkirka@gmail.com>
2019-12-20 19:32:39 +0300

    [SDAO-17] Add tests for multisig
    Problem: The multisig contract lacked proper testing.
    
    Solution: Add tests that cover a subset of possible usage scenarios
    (additional tests would be added later).

---
+++ contracts/Consumer.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+contract Consumer {
+    bytes11 public lastFixedBytes;
+    bytes public lastVarBytes;
+    uint104 public lastUint;
+    string public lastString;
+    uint public lastDataSize;
+
+    function updateData(
+        bytes11 fb,
+        bytes memory vb,
+        uint104 ui,
+        string memory str
+    ) public {
+        lastFixedBytes = fb;
+        lastVarBytes = vb;
+        lastUint = ui;
+        lastString = str;
+        lastDataSize = msg.data.length;
+    }
+
+}
---
+++ test/helpers.js
@@ -0,0 +1,59 @@
+const isRevert = error => {
+    const REVERT_MSGS = [
+        'Returned error: VM Exception while processing transaction: revert',
+        'Returned error: execution error: revert',
+    ]
+    return REVERT_MSGS.some(msg => error.message.startsWith(msg))
+}
+
+module.exports = {
+    isRevert: isRevert,
+    ignoreRevert: async promise => {
+        try {
+            return await promise
+        } catch (error) {
+            if (!isRevert(error)) {
+                throw error
+            }
+        }
+    },
+    assertRevert: async (promise, expectedReason) => {
+        try {
+            await promise
+        } catch (error) {
+            assert(
+                isRevert(error),
+                `Expected "revert", got ${error} instead`
+            )
+            assert.equal(
+                error.reason, expectedReason,
+                "The transaction failed with unexpected reason"
+            )
+
+            return
+        }
+        assert.fail('Expected revert not received')
+    },
+
+    fixSignature: signature => {
+        // in geth its always 27/28, in ganache its 0/1. Change to 27/28 to prevent
+        // signature malleability if version is 0/1
+        // see https://github.com/ethereum/go-ethereum/blob/v1.8.23/internal/ethapi/api.go#L465
+        let v = parseInt(signature.slice(130, 132), 16);
+        if (v < 27) {
+          v += 27;
+        }
+        const vHex = v.toString(16);
+        return signature.slice(0, 130) + vHex;
+    },
+
+    expectConsumerStorage: async (consumer, fields) => {
+        for (const [fieldName, expectedValue] of Object.entries(fields)) {
+            const actualValue = await consumer.contract.methods[fieldName]().call()
+            assert.equal(
+                actualValue, expectedValue,
+                `Field ${fieldName} has unexpected value`
+            )
+        }
+    },
+}

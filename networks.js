cab19a1fb5e40339c648e33a8a1920cd55336978
Greg Pevnev <grigory.pevnev@serokell.io>
2021-02-25 20:59:23 +0300

    [SDAO-333] Allow import account using plaintext secret key (#33)
    * [SDAO-333] Allow import account using plaintext secret key
    
    * Update README.md
    
    Co-authored-by: Kirill Kuvshinov <kkirka@gmail.com>
    
    Co-authored-by: George Agapov <george.agapov@gmail.com>
    Co-authored-by: Kirill Kuvshinov <kkirka@gmail.com>

--- networks.js
+++ networks.js
@@ -1,33 +1,46 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { getLedgerProvider } = require('./src/providers/ledger')
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { getLedgerProvider } = require("./src/providers/ledger");
 
 function env(variable) {
-  const value = process.env[variable]
+  const value = process.env[variable];
   if (value === undefined) {
-    throw new Error(`${variable} environment variable is not defined`)
+    throw new Error(`${variable} environment variable is not defined`);
   }
-  return value
+  return value;
 }
 
 function infuraUrl(network) {
-  const projectId = env('INFURA_PROJECT_ID')
-  return `https://${network}.infura.io/v3/${projectId}`
+  const projectId = env("INFURA_PROJECT_ID");
+  return `https://${network}.infura.io/v3/${projectId}`;
+}
+
+function chooseAccount() {
+  const mnemonic = process.env["DEV_MNEMONIC"];
+  const privateKey = process.env["DEV_SECRET_KEY"];
+  if (!(!!mnemonic ^ !!privateKey)) {
+    throw new Error("Please specify either DEV_MNEMONIC or DEV_SECRET_KEY");
+  }
+  return mnemonic ? { mnemonic } : { privateKeys: [privateKey] };
 }
 
 module.exports = {
   networks: {
-   development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*" // Any network (default: none)
     },
 
     goerli: {
-      provider: () => new HDWalletProvider(env('DEV_MNEMONIC'), infuraUrl('goerli')),
+      provider: () => {
+        var hdwProps = chooseAccount();
+        hdwProps.providerOrUrl = infuraUrl("goerli");
+        return new HDWalletProvider(hdwProps);
+      },
       network_id: 5,
       gas: 8000000,
-      gasPrice: 2000000000,  // 2 gwei
-    },
+      gasPrice: 2000000000 // 2 gwei
+    }
   },
 
   compilers: {
@@ -42,4 +55,4 @@ module.exports = {
       }
     }
   }
-}
+};
